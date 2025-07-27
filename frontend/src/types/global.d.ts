4275d96e076c9c441eda30b01a70536dba44ea7a
V01d42 <89440437+V01d42@users.noreply.github.com>
2025-07-27 21:12:55 +0900

    fix: change default.conf.template

--- frontend/src/types/global.d.ts
+++
@@ -1,9 +0,0 @@
-declare global {
-  interface Window {
-    appConfig?: {
-      VITE_API_BASE_URL: string;
-    };
-  }
-}
-
-export {}; 
--- frontend/src/utils/config.ts
+++ frontend/src/utils/config.ts
@@ -1,12 +1,5 @@
 export const getConfig = () => {
-  if (window.appConfig?.VITE_API_BASE_URL) {
-    return {
-      API_BASE_URL: window.appConfig.VITE_API_BASE_URL,
-    };
-  }
-
-  console.warn("Failed to fetch appConfig.js. Use default value.");
   return {
-    API_BASE_URL: "http://localhost:8080",
+    API_BASE_URL: "/api",
   };
 }; 
