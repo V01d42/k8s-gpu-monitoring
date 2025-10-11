a90bc23a3eb75c5e5ba24640209e94bfc6c68233
TomoyaOgawa <5stream418@gmail.com>
2025-10-11 14:24:51 +0900

    fix: change title, icon

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { useRoutes } from "react-router-dom";
-import "./App.css";
 import TopMenu from "./components/TopMenu";
 import routes from "./Routes";
 import { searchContext } from "./utils/contexts";
