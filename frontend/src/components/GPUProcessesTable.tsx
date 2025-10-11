db0a1601a1f3772b458b75f40a98b333be941d86
TomoyaOgawa <5stream418@gmail.com>
2025-10-11 16:49:19 +0900

    fix: add title and delete reload button

---
+++ frontend/src/components/GPUProcessesTable.tsx
@@ -0,0 +1,13 @@
+import { Typography } from "@mui/material";
+
+const GPUProcessesTable = () => {
+  return (
+    <>
+      <Typography variant="h4" sx={{ paddingTop: 2, marginBottom: 2 }}>
+        GPU Processes
+      </Typography>
+    </>
+  );
+};
+
+export default GPUProcessesTable;
