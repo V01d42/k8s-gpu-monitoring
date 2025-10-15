4aea2876b0ae04236be539107922fb4655e65981
HarutoS24 <harutosato2004@gmail.com>
2025-10-15 21:39:58 +0900

    change: removed children

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -2,12 +2,8 @@ import type { RouteObject } from "react-router-dom";
 import GPUMonitoringPage from "../pages/GPUMonitoringPage";
 
 const routes: RouteObject[] = [
-  {
-    children: [
-      { path: "/", element: <GPUMonitoringPage /> },
-      { path: "/healthz", element: <div>OK</div> },
-    ],
-  },
+  { path: "/", element: <GPUMonitoringPage /> },
+  { path: "/healthz", element: <div>OK</div> },
 ];
 
 export default routes;
